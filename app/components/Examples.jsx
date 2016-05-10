434418fc4b07d75c1e6dbd2964faa46194994fb5
szysyk <zag61728@gmail.com>
2016-05-10 22:32:53 +0900

    Add custom styles support

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -4,7 +4,7 @@ var {Link} = require('react-router');
 var Examples = (props) => {
   return (
     <div>
-      <h1 className="tet-center">Examples</h1>
+      <h1 className="text-center page-title">Examples</h1>
       <p>Here are a few example lacations to tyr out:</p>
       <ol>
         <li>
